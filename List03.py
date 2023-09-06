fb5beb27fa55d99557f9c8369d882f0596f53383
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-09-06 12:14:00 +0500

    nth

--- List03.py
+++ List03.py
@@ -1,6 +1,4 @@
 def main(list1,list2):
-    list1=[1,4,2,2]
-    list2=[dsds,g,d,s]
     list3=list1+list2
     """
     lists list1 and list2 are given. Add them (combine) and return.
@@ -11,4 +9,4 @@ def main(list1,list2):
         list: return answer
     """
     return list3
-print(main(range(list3)))
+print(main(['ss','fff','aa','dd'],['ddss','ff']))
