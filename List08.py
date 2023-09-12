ec17f2e249a4af0b0741ca8b7fc451797fa3f384
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-09-12 19:46:53 +0500

    nth

--- List08.py
+++ List08.py
@@ -1,4 +1,10 @@
 def main(list1):
+    ans=[]
+    for i in list(list1):
+        if int(i)==1:
+            ans+=[True]
+        if int(i)==0:
+            ans+=[False]
     """
     A list of ones and zeros, five in length, is given. replace one with True, replace zeros with False.
     Args:
@@ -6,4 +12,5 @@ def main(list1):
     Returns:
         list: return answer
     """
-    return
+    return ans
+print(main('10111'))
