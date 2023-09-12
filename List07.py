62148d061cfc70f5b06d77b1de5eb01c0f447371
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-09-12 19:45:48 +0500

    nth

--- List07.py
+++ List07.py
@@ -1,4 +1,10 @@
 def main(list1):
+    ans=[]
+    for i in list(list1):
+        if int(i)==1:
+            ans+=[1]
+        if int(i)==0:
+            ans+=[False]
     """
     A list of units and zeros with a length of five is given. Replace zero with False.
     Args:
@@ -6,4 +12,5 @@ def main(list1):
     Returns:
         list: return answer
     """
-    return
+    return ans
+print(main('10110'))
